8a543037e6a73b2f2df4fd248adfbfb593eafa3f
Theodore Omtzigt <theo@stillwater-sc.com>
2015-05-09 13:42:44 -0400

    Refactoring and renaming plays so it can be used for a larger site with more diversity in services

---
+++ plays/installs/scripts/mesosphere_repo.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+CODENAME=$(lsb_release -cs)
+echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
